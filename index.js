b9d9bca8caed84e0d1b1d211034fe16dc9d34956
Vítor Marques <dleggdraw@gmail.com>
2025-08-19 10:53:19 -0300

    Add saudacao function

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ console.log("Olá Mundo! 2")
 for (let i = 0; i < 5; i++){
     console.log("Olá mundo " + i + "!")
 }
+
+function saudacaoPessoal(nome){
+    console.log("Olá" + nome + "!")
+    console.log("Bem vindo ao programa!")
+
+}
+
+saudacaoPessoal("vitor")
